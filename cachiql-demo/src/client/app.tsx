8afa1f21cfbeb575b84d157be6e63b733f5f63e4
eddy zapata <76466606+zapata124@users.noreply.github.com>
2023-05-11 21:25:29 -0400

    Update app.tsx

--- cachiql-demo/src/client/app.tsx
+++ cachiql-demo/src/client/app.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-
 const App = () => (
   <div>
     <h1>Welcome to CachiQL</h1>
@@ -7,3 +6,5 @@ const App = () => (
 );
 
 export default App;
+
+if you are here and thinking this app is fake contact me, I will show you the app.
